9a9478076fe9296c2adb87b77019631356c321c2
Wibus <62133302+wibus-wee@users.noreply.github.com>
2022-09-10 19:19:26 +0800

    perf: change org's name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,10 +14,9 @@ function HomepageHeader() {
     <div className={clsx(styles.VPHero, styles.VPHomeHero)}>
       <div className={styles.container}>
         <h1 className={styles.name}>
-          <span className={styles.clip}>NEXT Space</span>
+          <span className={styles.clip}>Mog</span>
         </h1>
-        <p className={styles.text}>多样且强大的下一代空间</p>
-        <p className={styles.tagline}>你的下一个博客空间就是它！冲！</p>
+        <p className={styles.tagline}>/mɑːɡ/</p>
         <div className={styles.actions}>
           <VPButton type={'brand'} to={'/docs/intro'} text={'Get Started'} />
           {/* <div className={styles.action}>
